d682ade95c136c2c48de07f7e6c43aa753cc68f1
JeysonAmado <jeyson.amado@savne.net>
2023-12-21 10:09:28 -0500

    :tada: Feat: Initial commit

---
+++ src/test/java/JeysonAmadoA/FamilyMoney/FamilyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package JeysonAmadoA.FamilyMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
